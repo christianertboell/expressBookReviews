9a9d410c8d498328fb42ba7522ce256434f0f9b1
christianertboell <christian_ertboell@hotmail.com>
2023-08-10 04:49:21 -0400

    changes

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,31 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if there's an authorization object within the session
+    if (req.session && req.session.authorization) {
+        // Extract the access token from the session
+        const token = req.session.authorization.accessToken;
+        
+        // Verify the token
+        jwt.verify(token, "access", (err, decodedData) => {
+            // If there's no error and the token data is present
+            if (!err && decodedData) {
+                // Store the decoded data into the request for subsequent middleware/route handlers
+                req.user = decodedData;
+                next();  // proceed to the next middleware or route handler
+            } else {
+                // If there's an error during token verification
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        // If there's no authorization object in the session
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
+
+ 
  
 const PORT =5000;
 
